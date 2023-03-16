1e787661a35188a94db0dcb1a2194e99a2733412
Ruslan Pliev <ruslan-pliev@mail.ru>
2023-03-16 14:13:28 +0300

    -EntityScan -Transactional service

--- src/main/java/katapackage/service/UserServiceImpl.java
+++ src/main/java/katapackage/service/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package katapackage.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,7 +11,7 @@ import katapackage.model.User;
 import java.util.List;
 
 
-@Transactional
+
 @Service
 public class UserServiceImpl implements UserService {
 
