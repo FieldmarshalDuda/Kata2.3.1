a76c8939c9b1518a387ffa50975659fa73a42ffd
Ruslan Pliev <ruslan-pliev@mail.ru>
2023-03-18 13:32:36 +0300

    Кириллица

--- src/main/java/katapackage/service/UserServiceImpl.java
+++ src/main/java/katapackage/service/UserServiceImpl.java
@@ -3,13 +3,9 @@ package katapackage.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import katapackage.dao.UserDao;
 import katapackage.model.User;
-
-
 import java.util.List;
-
 
 
 @Service
@@ -20,32 +16,30 @@ public class UserServiceImpl implements UserService {
         userDao=dao;
     }
 
-
     @Override
+    @Transactional
     public void save(User user) {
         userDao.save(user);
     }
 
     @Override
+    @Transactional
     public void delete(int id) {
         userDao.delete(id);
     }
 
-
-
     @Override
+    @Transactional
     public void update(int id, User user) {
         userDao.update(id, user);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public List<User> getUsers() {
         return userDao.getUserList();
     }
 
     @Override
-    @Transactional(readOnly = true)
     public User show(int id) {
         return userDao.show(id);
     }
